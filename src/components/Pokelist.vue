69ce03be18e9c9b1c0d39c842e9d4dc6d496fa40
Anieb22 <anielloverdino@gmail.com>
2023-06-23 04:23:28 +0200

    aggiunto file store.js e inserito api al progetto

--- src/components/Pokelist.vue
+++ src/components/Pokelist.vue
@@ -1,27 +1,29 @@
 <script>
 import axios from 'axios'
+import { store } from '../store'
 export default {
     data() {
         return {
-            pokeList: []
+            store
         }
     },
     mounted() {
-        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons').then((response) => {
-            this.pokeList = response.data.docs;
+        axios.get(store.apiUrl).then((response) => {
+            store.pokeList = response.data.docs;
         })
     },
 }
 </script>
 <template lang="">
     <div class="col-12 d-flex flex-wrap justify-content-between">
-        <div v-for="(pokemon, index) in pokeList" :key="pokemon._id" class="card m-2 card-pokemon">
+        <div v-for="(pokemon, index) in store.pokeList" :key="pokemon._id" class="card m-2 card-pokemon">
   <img :src="pokemon.imageUrl" class="card-img-top rouded" alt="pokemon">
   <div class="card-body">
     <p class="card-text text-center">
         <h5>{{pokemon.number}}</h5>
-        <h3>{{pokemon.name}}</h3>
-        <h5>{{pokemon.type1}}</h5>
+        <h4>{{pokemon.name}}</h4>
+        <h5>{{pokemon.type1}} {{pokemon.type2}}</h5>
+        
     </p>
   </div>
 </div>
